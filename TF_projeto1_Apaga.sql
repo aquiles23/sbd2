02dded64e51fca53fe115eaf4efa304d56a781e4
aquiles <jjoseaquiless@gmail.com>
2018-06-19 19:35:31 -0300

    apaga

--- TF_projeto1_Apaga.sql
+++ TF_projeto1_Apaga.sql
@@ -1,5 +1,4 @@
-
--- --------            << PROJETO1 >>            ------------ --
+-- -------            << PROJETO1 >>            ------------ --
 --                                                                   --
 --                    SCRIPT  de criação(DDL)                        --
 --                                                                   --
@@ -12,34 +11,8 @@
 -- PROJETO => 1 Base de Dados                                        --
 --         => 2 tabelas                                              --
 -- ----------------------------------------------------------------- --
-create database NUMTA;
-use NUMTA;
 
-CREATE TABLE INFO (
-    filename VARCHAR(20) PRIMARY KEY NOT NULL,
-    original_filename VARCHAR(50),
-    scanid INT,
-    digit INT,
-    database_name_original VARCHAR(50),
-    contributing_team VARCHAR(50),
-    database_name VARCHAR(50),
-    num int,
-    districtid int,
-    institutionid int,
-    gender int,
-    age int,
-    datestamp int
-)  ENGINE=INNODB;
+USE NUMTA;
 
-
-CREATE TABLE IMAGEM (
-    imagem BLOB,
-    filename VARCHAR(20) UNIQUE NOT NULL,
-    CONSTRAINT info_imagem_FK FOREIGN KEY (filename)
-        REFERENCES INFO (filename)
-)  ENGINE=INNODB;
-
-
-
-        
-
+drop table IMAGEM;
+DROP TABLE INFO;
