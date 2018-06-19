aa4b14409b0851e176cd262df7c192851eb5a42e
aquiles <jjoseaquiless@gmail.com>
2018-06-19 01:47:37 -0300

    separando views

--- TF_projeto1_Fisico.sql
+++ TF_projeto1_Fisico.sql
@@ -10,9 +10,7 @@
 --                                                                   --
 --                                                                   --
 -- PROJETO => 1 Base de Dados                                        --
---         => 1 procedure                                           --
 --         => 2 tabelas                                              --
---         => 3 views                                                --
 -- ----------------------------------------------------------------- --
 create database if not exists NUMTA;
 use NUMTA;
@@ -42,71 +40,6 @@ CREATE TABLE IMAGEM (
 )  ENGINE=INNODB;
 
 
-CREATE VIEW TRAINING_A AS 
-	select i.filename,
-		i.original_filename, 
-		i.scanid, 
-        i.digit, 
-        i.database_name, 
-        i.database_name_original, 
-        i.contributing_team,
-        im.imagem 
-        FROM INFO i, IMAGEM im
-			where i.filename like 'a%' and i.filename = im.filename;
-            
-CREATE VIEW TRAINING_B AS
-	select i.filename,
-		i.original_filename, 
-		i.scanid, 
-        i.digit, 
-        i.database_name, 
-        i.database_name_original, 
-        i.contributing_team,
-        im.imagem
-        FROM INFO i, IMAGEM im
-			where i.filename like 'b%' and i.filename = im.filename;
 
-CREATE VIEW TRAINING_C AS
-	select i.filename,
-		i.original_filename, 
-		i.scanid, 
-        i.digit, 
-        i.database_name, 
-        i.database_name_original, 
-        i.contributing_team,
-        im.imagem
-        FROM INFO i, IMAGEM im
-			where i.filename like 'c%' and i.filename = im.filename;
-
-CREATE VIEW TRAINING_D AS
-	select i.filename,
-		i.original_filename, 
-		i.scanid, 
-        i.digit, 
-        i.database_name,
-        i.num,
-        i.database_name_original, 
-        i.contributing_team,
-        im.imagem
-        FROM INFO i, IMAGEM im
-			where i.filename like 'd%' and i.filename = im.filename;
-
-CREATE VIEW TRAINING_E AS
-	select i.filename,
-		i.original_filename, 
-		i.scanid, 
-        i.digit,
-        i.districtid,
-		i.institutionid,
-		i.gender,
-		i.age,
-		i.datestamp,
-        i.database_name, 
-        i.database_name_original,
-        im.imagem
-        FROM INFO i, IMAGEM im
-			where i.filename like 'e%' and i.filename = im.filename;
         
-            
 
-select * from TRAINING_E;
---
+++ TF_projeto1_Views.sql
@@ -0,0 +1,78 @@
+-- --------            << PROJETO1 >>            ------------ --
+--                                                                   --
+--                    SCRIPT  de criação(DDL)                        --
+--                                                                   --
+-- Data Criacao ...........: 13/10/2017                              --
+-- Autor(es) ..............: José Aquiles , Ramon           --
+-- Banco de Dados .........: MySQL                                   --
+-- Banco de Dados(nome) ...: NUMTA                                  --
+--                                                                   --
+--                                                                   --
+-- PROJETO => 1 Base de Dados                                        --
+--         => 2 tabelas                                              --
+-- ----------------------------------------------------------------- --
+
+CREATE VIEW TRAINING_A AS 
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name, 
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem 
+        FROM INFO i, IMAGEM im
+			where i.filename like 'a%' and i.filename = im.filename;
+            
+CREATE VIEW TRAINING_B AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name, 
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'b%' and i.filename = im.filename;
+
+CREATE VIEW TRAINING_C AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name, 
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'c%' and i.filename = im.filename;
+
+CREATE VIEW TRAINING_D AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name,
+        i.num,
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'd%' and i.filename = im.filename;
+
+CREATE VIEW TRAINING_E AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit,
+        i.districtid,
+		i.institutionid,
+		i.gender,
+		i.age,
+		i.datestamp,
+        i.database_name, 
+        i.database_name_original,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'e%' and i.filename = im.filename;
