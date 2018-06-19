4550998b4099c057d67a4f33eae09b74ae367b33
aquiles <jjoseaquiless@gmail.com>
2018-06-18 23:41:15 -0300

    mais view

--- TF_projeto1_Fisico.sql
+++ TF_projeto1_Fisico.sql
@@ -49,8 +49,64 @@ CREATE VIEW TRAINING_A AS
         i.digit, 
         i.database_name, 
         i.database_name_original, 
-        i.contributing_team 
+        i.contributing_team,
+        im.imagem 
         FROM INFO i, IMAGEM im
 			where i.filename like 'a%' and i.filename = im.filename;
             
+CREATE VIEW TRAINING_B AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name, 
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'b%' and i.filename = im.filename;
 
+CREATE VIEW TRAINING_C AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name, 
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'c%' and i.filename = im.filename;
+
+CREATE VIEW TRAINING_D AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit, 
+        i.database_name,
+        i.num,
+        i.database_name_original, 
+        i.contributing_team,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'd%' and i.filename = im.filename;
+
+CREATE VIEW TRAINING_E AS
+	select i.filename,
+		i.original_filename, 
+		i.scanid, 
+        i.digit,
+        i.districtid,
+		i.institutionid,
+		i.gender,
+		i.age,
+		i.datestamp,
+        i.database_name, 
+        i.database_name_original,
+        im.imagem
+        FROM INFO i, IMAGEM im
+			where i.filename like 'e%' and i.filename = im.filename;
+        
+            
+
+select * from TRAINING_E;
